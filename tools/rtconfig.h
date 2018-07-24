d695856b465758dee3587e5ce3f55872e77c12a7
liu2guang <1004383796@qq.com>
2018-07-24 17:03:40 +0800

    [Test] Test .travis.yml file validity. | 测试.travis.yml文件的正确性. (普通用户无需关心)

--- tools/rtconfig.h
+++ tools/rtconfig.h
@@ -10,12 +10,13 @@
 #define RT_ALIGN_SIZE 4
 #define RT_THREAD_PRIORITY_32
 #define RT_THREAD_PRIORITY_MAX 32
-#define RT_TICK_PER_SECOND 1000
+#define RT_TICK_PER_SECOND 100
 #define RT_DEBUG
 #define RT_USING_OVERFLOW_CHECK
 #define RT_DEBUG_INIT 0
 #define RT_DEBUG_THREAD 0
 #define RT_USING_HOOK
+#define RT_IDEL_HOOK_LIST_SIZE 4
 #define IDLE_THREAD_STACK_SIZE 256
 
 /* Inter-Thread communication */
@@ -64,6 +65,7 @@
 #define FINSH_CMD_SIZE 80
 #define FINSH_USING_MSH
 #define FINSH_USING_MSH_DEFAULT
+#define FINSH_ARG_MAX 10
 
 /* Device virtual file system */
 
@@ -89,12 +91,18 @@
 /* Device Drivers */
 
 #define RT_USING_DEVICE_IPC
+#define RT_PIPE_BUFSZ 512
 #define RT_USING_SERIAL
 #define RT_USING_I2C
 #define RT_USING_I2C_BITOPS
 #define RT_USING_PIN
 #define RT_USING_RTC
 #define RT_USING_SDIO
+#define RT_SDIO_STACK_SIZE 512
+#define RT_SDIO_THREAD_PRIORITY 15
+#define RT_MMCSD_STACK_SIZE 1024
+#define RT_MMCSD_THREAD_PREORITY 22
+#define RT_MMCSD_MAX_PARTITION 16
 #define RT_USING_SPI
 
 /* Using USB */
@@ -108,6 +116,41 @@
 
 /* light weight TCP/IP stack */
 
+#define RT_USING_LWIP
+#define RT_USING_LWIP202
+#define RT_LWIP_ICMP
+#define RT_LWIP_DNS
+#define RT_LWIP_DHCP
+#define IP_SOF_BROADCAST 1
+#define IP_SOF_BROADCAST_RECV 1
+
+/* Static IPv4 Address */
+
+#define RT_LWIP_IPADDR "192.168.1.30"
+#define RT_LWIP_GWADDR "192.168.1.1"
+#define RT_LWIP_MSKADDR "255.255.255.0"
+#define RT_LWIP_UDP
+#define RT_LWIP_TCP
+#define RT_MEMP_NUM_NETCONN 8
+#define RT_LWIP_PBUF_NUM 16
+#define RT_LWIP_RAW_PCB_NUM 4
+#define RT_LWIP_UDP_PCB_NUM 4
+#define RT_LWIP_TCP_PCB_NUM 4
+#define RT_LWIP_TCP_SEG_NUM 40
+#define RT_LWIP_TCP_SND_BUF 8196
+#define RT_LWIP_TCP_WND 8196
+#define RT_LWIP_TCPTHREAD_PRIORITY 10
+#define RT_LWIP_TCPTHREAD_MBOX_SIZE 8
+#define RT_LWIP_TCPTHREAD_STACKSIZE 1024
+#define RT_LWIP_ETHTHREAD_PRIORITY 12
+#define RT_LWIP_ETHTHREAD_STACKSIZE 1024
+#define RT_LWIP_ETHTHREAD_MBOX_SIZE 8
+#define LWIP_NETIF_STATUS_CALLBACK 1
+#define SO_REUSE 1
+#define LWIP_SO_RCVTIMEO 1
+#define LWIP_SO_SNDTIMEO 1
+#define LWIP_SO_RCVBUF 1
+#define LWIP_NETIF_LOOPBACK 0
 
 /* Modbus master and slave stack */
 
@@ -122,24 +165,6 @@
 
 
 /* RT-Thread online packages */
-
-/* system packages */
-
-/* RT-Thread GUI Engine */
-
-#define PKG_USING_LITTLEVGL2RTT
-#define PKG_USING_LITTLEVGL2RTT_LATEST_VERSION
-
-/* LittlevGL2RTT Options */
-
-#define LV_MEM_DYNAMIC
-#define LV_MEM_CUSTOM 1
-#define LV_COLOR_DEPTH_16
-#define LV_COLOR_DEPTH 16
-#define LV_HOR_RES 800
-#define LV_VER_RES 480
-#define LV_DPI 50
-#define LITTLEVGL2RTT_USING_DEMO
 
 /* IoT - internet of things */
 
@@ -164,12 +189,35 @@
 /* tools packages */
 
 
+/* system packages */
+
+#define PKG_USING_LITTLEVGL2RTT
+#define PKG_USING_LITTLEVGL2RTT_V001
+
+/* LittlevGL2RTT Options */
+
+#define LV_MEM_DYNAMIC
+#define LV_MEM_CUSTOM 1
+#define LV_COLOR_DEPTH_16
+#define LV_COLOR_DEPTH 16
+#define LV_HOR_RES 480
+#define LV_VER_RES 272
+#define LV_DPI 50
+#define LITTLEVGL2RTT_USING_DEMO
+
+/* peripheral libraries and drivers */
+
+
 /* miscellaneous packages */
+
+
+/* sample package */
 
 
 /* example package: hello */
 
 #define SOC_IMXRT1052
+#define BOARD_USING_QSPIFLASH
 #define BOARD_RT1050_FIRE
 
 /* RT1050 Bsp Config */
@@ -186,27 +234,22 @@
 
 /* Select iic bus drivers */
 
-#define RT_USING_HW_I2C1
-#define HW_I2C1_BADURATE_400kHZ
 
 /* Select lcd driver */
 
 /* Notice: Fire Board para: 800*480 4 4 8 2 40 10 58 45 */
 
 #define RT_USING_LCD
-#define LCD_WIDTH 800
-#define LCD_HEIGHT 480
+#define LCD_WIDTH 480
+#define LCD_HEIGHT 272
 #define LCD_HFP 4
 #define LCD_VFP 4
 #define LCD_HBP 8
 #define LCD_VBP 2
 #define LCD_HSW 40
 #define LCD_VSW 10
-#define LCD_BL_PIN 58
+#define LCD_BL_PIN 106
 #define LCD_RST_PIN 45
 #define RT_USING_SDRAM
-#define RT_USING_RTC_HP
-
-#define PKG_USING_TOUCH
 
 #endif
